14ceb67e715d515952af14e92678f51f975abb7b
Akshit0310 <96770400+Akshit0310@users.noreply.github.com>
2022-01-07 23:32:42 +0530

    Add files via upload

---
+++ python program output/6. replacing Black and Pink to Purple output.py
@@ -0,0 +1,5 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 18:54:59) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: D:/pec/study/computer/python program/Q5b)_input_replacing Black and Pink to Purple.py
+['Red', 'Green', 'White', 'Purple', 'Yellow']
