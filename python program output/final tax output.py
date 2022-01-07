d24891ba4b0690fad0a0b74a4f201e2419facba0
Akshit0310 <96770400+Akshit0310@users.noreply.github.com>
2022-01-07 23:29:22 +0530

    Delete final tax output.py

--- python program output/final tax output.py
+++
@@ -1,7 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 18:54:59) [MSC v.1929 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-========================================================== RESTART: D:/pec/study/computer/python program/Q2_input_final tax.py =========================================================
-Enter your Gross Income(in nearest penny): $100000
-Enter number of your Dependents: 3
-Your tax is $ 16200.0
