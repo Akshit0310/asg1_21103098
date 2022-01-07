ba05cefc50ca42120756437d2d792cd3a41f2e36
Akshit0310 <96770400+Akshit0310@users.noreply.github.com>
2022-01-07 23:29:45 +0530

    Delete removing black output.py

--- python program output/removing black output.py
+++
@@ -1,5 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 18:54:59) [MSC v.1929 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-== RESTART: D:/pec/study/computer/python program/Q5a)_input_removing black.py ==
-['Red', 'Green', 'White', 'Pink', 'Yellow']
