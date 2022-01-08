c8e538aa3ea41d111bcf29548abcb1d1d765f974
Akshit0310 <96770400+Akshit0310@users.noreply.github.com>
2022-01-08 13:50:30 +0530

    Delete Q2_input_final tax.py

--- python program/Q2_input_final tax.py
+++
@@ -1,6 +0,0 @@
-
-a=int(input("Enter your Gross Income(in nearest penny): $"))  #taking gross income input from user
-b=int(input("Enter number of your Dependents: "))  #number of dependents given by user
-c=a-10000-(3000*b)  # calculating tax income
-Tax=c*0.2  #final tax
-print("Your tax is $",Tax)
