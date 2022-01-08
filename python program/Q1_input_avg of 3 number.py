953039def22a3d16e89308e80c4d3f7ff6c230c3
Akshit0310 <96770400+Akshit0310@users.noreply.github.com>
2022-01-08 13:50:23 +0530

    Delete Q1_input_avg of 3 number.py

--- python program/Q1_input_avg of 3 number.py
+++
@@ -1,7 +0,0 @@
-#taking input from user
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-num3=int(input("Enter third number: "))
-d=(num1+num2+num3)/3    #calculating average
-print("Average of three numbers is: ",d)
-
